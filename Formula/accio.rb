fc4bfd4ce077707313539aead055ce6c44b48993
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2019-03-08 21:58:25 +0100

    Add commit hash to Homebrew formula

--- Formula/accio.rb
+++ Formula/accio.rb
@@ -1,7 +1,7 @@
 class Accio < Formula
   desc "Dependency manager driven by SwiftPM for iOS & Co."
   homepage "https://github.com/JamitLabs/Accio"
-  url "https://github.com/JamitLabs/Accio.git", :tag => "0.1.0", :revision => "???"
+  url "https://github.com/JamitLabs/Accio.git", :tag => "0.1.0", :revision => "fb91e051e57f93671e7053acfba31f807b651835"
   head "https://github.com/JamitLabs/Accio.git"
 
   depends_on :xcode => ["10.0", :build]
