2cee1878ba33e658d11a3e4de3dcdb7ccb97ddff
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2019-03-29 12:24:06 +0100

    Update formula to version 0.4.0

--- Formula/accio.rb
+++ Formula/accio.rb
@@ -1,7 +1,7 @@
 class Accio < Formula
   desc "Dependency manager driven by SwiftPM for iOS & Co."
   homepage "https://github.com/JamitLabs/Accio"
-  url "https://github.com/JamitLabs/Accio.git", :tag => "0.3.0", :revision => "ae23f936a93017da790cab1f90ec95bd0486071d"
+  url "https://github.com/JamitLabs/Accio.git", :tag => "0.4.0", :revision => "3fedfcd1d62705566973d25883a72c698b32431a"
   head "https://github.com/JamitLabs/Accio.git"
 
   depends_on :xcode => ["10.2", :build]
