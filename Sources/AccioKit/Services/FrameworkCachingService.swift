5ee7041a7993e5d252183b232b64cd6e063389bb
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2019-03-31 20:21:04 +0200

    Elegantly handle cases where shared cache isn't available

--- Sources/AccioKit/Services/FrameworkCachingService.swift
+++ Sources/AccioKit/Services/FrameworkCachingService.swift
@@ -32,7 +32,7 @@ final class FrameworkCachingService {
     func cache(product: FrameworkProduct, framework: Framework, platform: Platform) throws {
         let subpath: String = cacheFileSubPath(framework: framework, platform: platform)
 
-        if let sharedCachePath = sharedCachePath {
+        if let sharedCachePath = sharedCachePath, FileManager.default.fileExists(atPath: sharedCachePath) {
             try cache(product: product, to: URL(fileURLWithPath: sharedCachePath).appendingPathComponent(subpath))
             print("Saved build products for \(framework.libraryName) in shared cache.", level: .info)
         } else {
