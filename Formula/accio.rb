069018f5275a4d37d20e8859120f3ecd7e35f550
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2019-04-05 17:14:21 +0200

    Update Homebrew formula to pass audit

--- Formula/accio.rb
+++ Formula/accio.rb
@@ -1,5 +1,5 @@
 class Accio < Formula
-  desc "Dependency manager driven by SwiftPM for iOS & Co."
+  desc "Dependency manager driven by SwiftPM for iOS/macOS/tvOS/watchOS"
   homepage "https://github.com/JamitLabs/Accio"
   url "https://github.com/JamitLabs/Accio.git", :tag => "0.5.5", :revision => "3b8a910fc4e627c81ffa2e4eafeff450136bb0e4"
   head "https://github.com/JamitLabs/Accio.git"
@@ -8,5 +8,9 @@ class Accio < Formula
 
   def install
     system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system bin/"accio", "version"
   end
 end
