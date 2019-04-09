e39dd10bfbf34f15f414546844903c8432d550c6
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2019-04-09 14:57:00 +0200

    Improve wording in output to be more precise

--- Sources/AccioKit/Services/CarthageBuilderService.swift
+++ Sources/AccioKit/Services/CarthageBuilderService.swift
@@ -13,7 +13,7 @@ final class CarthageBuilderService {
     }
 
     func build(framework: Framework, platform: Platform, alreadyBuiltFrameworkProducts: [FrameworkProduct]) throws -> FrameworkProduct {
-        print("Building scheme \(framework.libraryName) with Carthage ...", level: .info)
+        print("Building library \(framework.libraryName) with Carthage ...", level: .info)
 
         try bash("git -C '\(framework.projectDirectory)' reset HEAD --hard --quiet")
         try bash("git -C '\(framework.projectDirectory)' clean -fd --quiet")
