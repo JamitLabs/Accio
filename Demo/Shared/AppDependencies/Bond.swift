bcfd95e430dbc272610172a97471e57e2185dea6
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2019-04-11 16:07:32 +0200

    Re-enable Bond integration testing

--- Demo/Shared/AppDependencies/Bond.swift
+++ Demo/Shared/AppDependencies/Bond.swift
@@ -1,4 +1,4 @@
-//import Bond
-//
-//// Ensure that framework was correctly integrated by using public API:
-//let bondIntType = Bond<Int>.self
+import Bond
+
+// Ensure that framework was correctly integrated by using public API:
+let bondIntType = Bond<Int>.self
