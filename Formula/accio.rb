1d661df890316bec78c32e03747847e6a993fb5d
Frederick Pietschmann <19194800+fredpi@users.noreply.github.com>
2019-06-20 14:35:26 +0200

    Update Homebrew formula to version 0.6.2

--- Formula/accio.rb
+++ Formula/accio.rb
@@ -1,7 +1,7 @@
 class Accio < Formula
   desc "Dependency manager driven by SwiftPM for iOS/macOS/tvOS/watchOS"
   homepage "https://github.com/JamitLabs/Accio"
-  url "https://github.com/JamitLabs/Accio.git", :tag => "0.6.1", :revision => "6b01bd6458c71eb946402144d6c21e3a855aadac"
+  url "https://github.com/JamitLabs/Accio.git", :tag => "0.6.2", :revision => "b9bb6215778fccc6c4428101786b582f9217cd75"
   head "https://github.com/JamitLabs/Accio.git"
 
   depends_on :xcode => ["10.2", :build]
